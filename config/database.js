e364cf0e9a7e5553aa2ad60f98e9936fd1d7fe44
andrei gagiu <gagiumandrei@gmail.com>
2019-03-25 11:04:49 +0000

    ok

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb+srv://andreigagiu:fender453@cluster0-afutq.mongodb.net/test?retryWrites=true'}
+  module.exports = {mongoURI: 'mongodb://andreigagiu:fender453@cluster0-afutq.mongodb.net/test?retryWrites=true'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
