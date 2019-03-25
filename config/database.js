ee81270584d87ab4623ebb07ef84061c3697f3fd
andrei gagiu <gagiumandrei@gmail.com>
2019-03-25 11:51:03 +0000

    done

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://andreigagiu:andrei@cluster0-afutq.mongodb.net/test?retryWrites=true'}
+  module.exports = {mongoURI: 'mongodb://andrei:fenderslashandrei@SG-vdprod-19749.servers.mongodirector.com:48065,SG-vdprod-19748.servers.mongodirector.com:48065,SG-vdprod-19750.servers.mongodirector.com:48065/admin?replicaSet=RS-vdprod-0&ssl=true'}
 }else{
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
